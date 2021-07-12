7aa7eadf8d51b900cd6ce34173e8483819f6d015
Thomas Begush <tbegush@gmail.com>
2021-07-11 20:32:34 -0700

    instanciating DB access

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'L4dybug5',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 // app.get('/', (req, res) => {
 //     res.json({
 //       message: 'Hello World'
 //     });
 //   });
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
